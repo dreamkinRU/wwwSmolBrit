98ec25b84756b8cba0802fc45e527c9c5e6aedad
dreamkinRU <12631738+dreamkinRU@users.noreply.github.com>
2019-01-09 02:03:19 +0300

    edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -206,7 +206,7 @@
 										.css('top', '');
 								else
 									$sidebar_inner
-										.css('top', -1 * x);
+										.css('top', '');
 
 							}
 							else {
@@ -215,7 +215,7 @@
 									$sidebar_inner
 										.data('locked', 1)
 										.css('position', 'fixed')
-										.css('top', -1 * x);
+										.css('top', '');
 
 							}
 
